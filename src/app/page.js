e013ca8a3c35a78887cca3caf661a40868842d85
Wolcken_Git <Alfredoxd00@hotmail.com>
2023-06-14 00:32:51 -0400

    Implementing ChatGPT API

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,148 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+// import Image from 'next/image'
+// import styles from './page.module.css'
+import { useState } from 'react'
+import axios from 'axios';
 
 export default function Home() {
+
+  const [inputValue, setInputValue] = useState('');
+  const [chatLog, setChatLog] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    setChatLog((prevChatLog) => [...prevChatLog, { type: 'user', message: inputValue }]);
+
+    sendMessage(inputValue);
+
+    setInputValue('');
+  }
+
+  const sendMessage = (message) => {
+    const url = 'https://api.openai.com/v1/chat/completions';
+    const headers = {
+      'Content-type': 'application/json',
+      'Authorization': `Bearer ${process.env.NEXT_PUBLIC_OPENAI_API_KEY}`
+    };
+    const data = {
+      model: 'gpt-3.5-turbo-0301',
+      messages: [{ 'role': 'user', 'content': message }]
+    };
+
+    setIsLoading(true);
+
+    axios.post(url, data, { headers: headers }).then((response) => {
+      console.log(response);
+      setChatLog((prevChatLog) => [...prevChatLog, { type: 'bot', message: response.data.choices[0].message.content }]);
+      setIsLoading(false);
+    }).catch((error) => {
+      setIsLoading(false);
+      console.log(error)
+    })
+  }
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <>
+      <div>Wolcken Chat GPT</div>
+      {
+        chatLog.map((message, index) => (
+          <div key={index}>{message.message}</div>
+        ))
+      }
+      <form onSubmit={handleSubmit}>
+        <input type='text' placeholder='Type your message...' value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
+        <button type='submit'>Send</button>
+      </form>
+    </>
+    // <main className={styles.main}>
+    //   <div className={styles.description}>
+    //     <p>
+    //       Get started by editing&nbsp;
+    //       <code className={styles.code}>src/app/page.js</code>
+    //     </p>
+    //     <div>
+    //       <a
+    //         href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+    //         target="_blank"
+    //         rel="noopener noreferrer"
+    //       >
+    //         By{' '}
+    //         <Image
+    //           src="/vercel.svg"
+    //           alt="Vercel Logo"
+    //           className={styles.vercelLogo}
+    //           width={100}
+    //           height={24}
+    //           priority
+    //         />
+    //       </a>
+    //     </div>
+    //   </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+    //   <div className={styles.center}>
+    //     <Image
+    //       className={styles.logo}
+    //       src="/next.svg"
+    //       alt="Next.js Logo"
+    //       width={180}
+    //       height={37}
+    //       priority
+    //     />
+    //   </div>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+    //   <div className={styles.grid}>
+    //     <a
+    //       href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+    //       className={styles.card}
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       <h2>
+    //         Docs <span>-&gt;</span>
+    //       </h2>
+    //       <p>Find in-depth information about Next.js features and API.</p>
+    //     </a>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+    //     <a
+    //       href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+    //       className={styles.card}
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       <h2>
+    //         Learn <span>-&gt;</span>
+    //       </h2>
+    //       <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
+    //     </a>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
+    //     <a
+    //       href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+    //       className={styles.card}
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       <h2>
+    //         Templates <span>-&gt;</span>
+    //       </h2>
+    //       <p>Explore the Next.js 13 playground.</p>
+    //     </a>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    //     <a
+    //       href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+    //       className={styles.card}
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       <h2>
+    //         Deploy <span>-&gt;</span>
+    //       </h2>
+    //       <p>
+    //         Instantly deploy your Next.js site to a shareable URL with Vercel.
+    //       </p>
+    //     </a>
+    //   </div>
+    // </main>
   )
 }
